5095ea79423edcba85a8417c234c34be5ca08382
val <me@here.com>
2016-09-29 13:08:45 +0300

    remapped functions from inner namespace

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+from .conventional.bicoherence import *
+from .conventional.bicoherencex import *
+from .conventional.bispectrumd import *
+from .conventional.bispectrumdx import *
+from .conventional.bispectrumi import *
+from .conventional.cum2est import *
+from .conventional.cum2x import *
+from .conventional.cum3est import *
+from .conventional.cum3x import *
+from .conventional.cum4est import *
+from .conventional.cum4x import *
+from .conventional.cumest import *
+del test
