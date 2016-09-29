7f4c3208a2240e5881441f5a61cd3136aca901ae
val <me@here.com>
2016-09-29 14:16:15 +0300

    setup.py modified

--- setup.py
+++ setup.py
@@ -2,26 +2,9 @@
 
 from setuptools import setup, find_packages
 
-
 setup(
-  name = "spectrum",
-  version = "0.1",
-  packages = find_packages(),
-  # scripts = ['say_hello.py'],
-
-  install_requires = ['numpy>=1.8', 'scipy>0.13.3', 'matplotlib>1.3.1'],
-
-  include_package_data = True,
-
-  package_data = {
-    '': ['*.txt', '*.rst', '*.mat'],
-  },
-
-  # metadata for upload to PyPI
-  author = "ixaxaar",
-  author_email = "root@ixaxaar.in",
-  description = "Higher Order Spectrum Estimation toolkit",
-  license = "MIT",
-  keywords = "Higher Order Spectrum Estimation toolkit",
-  url = "https://github.com/synergetics/spectrum",
+    name="hospectrum",
+    version="0.1",
+    packages=find_packages(exclude=['*.demo']),
+    install_requires=['numpy>=1.8', 'scipy>0.13.3', 'matplotlib>1.3.1']
 )
